bb4bbdd0bc1e508fccc0ecada105afc3faae43d4
Nathan Lima <137341056+NathanLima1@users.noreply.github.com>
2025-01-22 11:33:12 -0300

    Atualizar o extensor_sinal.vhd
    downto

--- extensor_sinal.vhd
+++ extensor_sinal.vhd
@@ -4,14 +4,14 @@ use IEEE.numeric_std.all;
 
 entity ExtSinal is 
     port(
-        input_16: in std_logic_vector(15 down to 0);
-        output_32: out std_logic_vector(31 down to 0);
+        input_16: in std_logic_vector(15 downto 0);
+        output_32: out std_logic_vector(31 downto 0);
     );
 end ExtSinal;
 
 architecture GenerateBased of ExtSinal is
     begin
-    output_32(15 down to 0) <= input_16;
+    output_32(15 downto 0) <= input_16;
 
     gen: for i in 16 to 31 generate
         output_32 <= input_16(15);
