1b73cc2bf6be0ed97047199a802bc2d2f54fde4c
Nathan Lima <137341056+NathanLima1@users.noreply.github.com>
2025-01-24 08:04:41 -0300

    Update extensor_sinal.vhd

--- extensor_sinal.vhd
+++ extensor_sinal.vhd
@@ -1,15 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Nathan Lima
+-- 
+-- Create Date:    07:59:36 01/24/2025 
+-- Design Name: 
+-- Module Name:    extensor_sinal - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 
-entity ExtSinal is 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity extensor_sinal is 
     port(
         input_16: in std_logic_vector(15 downto 0);
-        output_32: out std_logic_vector(31 downto 0);
+        output_32: out std_logic_vector(31 downto 0)
     );
-end ExtSinal;
+end extensor_sinal;
 
-architecture GenerateBased of ExtSinal is
+architecture GenerateBased of extensor_sinal is
     begin
     output_32(15 downto 0) <= input_16;
 
@@ -17,3 +45,4 @@ architecture GenerateBased of ExtSinal is
         output_32 <= input_16(15);
     end generate;
 end GenerateBased;
+
