fbd72ba6398cde573ce0d0f467eb32730f6a9a4f
Vítor Oliveira Amorim <135454159+Voamorim@users.noreply.github.com>
2025-02-12 09:03:43 -0300

    Atualiza a ULA de 32 bits

--- Ula_32.vhd
+++ Ula_32.vhd
@@ -18,65 +18,62 @@ end Ula_32;
 
 architecture Behavioral of Ula_32 is
 	signal VaiUm : std_logic_vector(31 downto 0);
-   signal OrResult : std_logic;
+   	signal OrResult : std_logic;
 	signal VaiSet : std_logic;
 	signal ResultadoLocal : std_logic_vector (31 downto 0);
 begin
-	-- Generate das ULAs de 1 bit
+	-- Instanciação da primeira ULA de 1 bit. 
    	Ula0_1 : entity work.ula_1
-			port map (
-            A => A(0),
-            B => B(0),
-            Ainvert => Anegate,
-            Binvert => Bnegate,
-            VemUm => Bnegate, -- Adiciona 1 caso a operacao seja de subtracao
-            Operacao => Operacao,
-            Resultado => ResultadoLocal(0),
-            VaiUm => VaiUm(0),
-				
-				-- Corrigir
-				Less => VaiSet
-         );
+	    port map (
+            	A => A(0),
+            	B => B(0),
+            	Ainvert => Anegate,
+            	Binvert => Bnegate,
+            	VemUm => Bnegate, -- Adiciona 1 caso a operacao seja de subtracao
+            	Operacao => Operacao,
+            	Resultado => ResultadoLocal(0),
+            	VaiUm => VaiUm(0),
+	    	Less => VaiSet
+            );
 
-		
-		gen_ula : for i in 1 to 30 generate
-    	begin
-            Ula0_resto : entity work.ula_1
-                port map (
-                   A => A(i),
-                   B => B(i),
-                   Ainvert => Anegate,
-                   Binvert => Bnegate,
-                   VemUm => VaiUm(i-1),
-                   Operacao => Operacao,
-                   Resultado => ResultadoLocal(i),
-                   VaiUm => VaiUm(i),
-						 Less => '0'
-                );
-		end generate;
-		
-		Ula31_1 : entity work.ula_1
-			port map (
-				A => A(31),
-				B => B(31),
-				Ainvert => Anegate,
-				Binvert => Bnegate,
-				VemUm => VaiUm(30),
-				Operacao => Operacao,
-				Resultado => ResultadoLocal(31),
-				VaiUm => VaiUm(31),
-				Less => '0',
-				Set => VaiSet
-			);
+	-- Generate das ULAs dos bits de 1 - 30
+	gen_ula : for i in 1 to 30 generate
+    	    begin
+            	Ula0_resto : entity work.ula_1
+                   port map (
+                      A => A(i),
+                      B => B(i),
+                      Ainvert => Anegate,
+                      Binvert => Bnegate,
+                      VemUm => VaiUm(i-1),
+                      Operacao => Operacao,
+                      Resultado => ResultadoLocal(i),
+                      VaiUm => VaiUm(i),
+		      Less => '0'
+                   );
+	    end generate;
+
+	-- Instanciação da última ULA de 1 bit
+	Ula31_1 : entity work.ula_1
+	   port map (
+	      A => A(31),
+	      B => B(31),
+	      Ainvert => Anegate,
+	      Binvert => Bnegate,
+	      VemUm => VaiUm(30),
+	      Operacao => Operacao,
+              Resultado => ResultadoLocal(31),
+	      VaiUm => VaiUm(31),
+	      Less => '0',
+	      Set => VaiSet
+	   );
 	
-		Or_32Instancia : entity work.or_32
-			port map (
-				   E => ResultadoLocal,
-					Saida => OrResult
-			  );
+	Or_32Instancia : entity work.or_32
+	   port map (
+	      E => ResultadoLocal,
+	      Saida => OrResult
+	   );
 			
-			Resultado <= ResultadoLocal;
-		 Zero <= not OrResult;
+	Resultado <= ResultadoLocal;
+	Zero <= not OrResult;
 end Behavioral;
-
-
