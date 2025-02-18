81f09f1c30c6059d3629b29831dbd37e4a9dbd10
Vítor Oliveira Amorim <135454159+Voamorim@users.noreply.github.com>
2025-02-17 21:13:01 -0300

    Novo formato a ser testado

--- caminho_de_dados.vhd
+++ caminho_de_dados.vhd
@@ -9,186 +9,6 @@ entity caminho_de_dados is
 end caminho_de_dados;
 
 architecture Behavioral of caminho_de_dados is
-	-- Registrador PC (Program Counter)
-	component PC
-		port(
-			-- Entradas
-			CLK: in std_logic;
-			Clear: in std_logic;
-			Enable: in std_logic;
-			Reset: in std_logic;
-			D_in: in std_logic_vector(31 downto 0);
-			
-			-- Saídas
-			Q_out: out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	-- Memória de instruções
-	component memInstrucoes 
-		Port(
-			-- Entradas
-			Endereco: in std_logic_vector(31 downto 0);
-			
-			-- Saídas
-			Palavra: out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	-- Somador de 32 bits 
-	component Somador32bits
-		Port(
-			-- Entradas
-			A: in std_logic_vector(31 downto 0);
-			B: in std_logic_vector (31 downto 0);
-			
-			-- Saídas
-			Soma: out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	-- Deslocador de 2 bits para a esquerda
-	component deslocador_2b_esq
-		Port(
-			-- Entradas
-			Entrada: in std_logic_vector(31 downto 0);
-			
-			-- Saídas
-			Saida: out std_logic_vector(31 downto 0)
-		);
-	end component;
-
-	-- Mux de 2 entradas de 1 bit
-	component mux_2 
-		Port(
-			-- Entradas
-			A: in std_logic;
-			B: in std_logic;
-			Sel: in std_logic;
-			
-			-- Saídas
-			Saida: out std_logic
-		);
-	end component;
-	
-	-- Mux de 2 entradas de 32 bits
-	component mux_2_x_32
-		Port(
-			-- Entradas
-			A: in std_logic_vector(31 downto 0);
-			B: in std_logic_vector(31 downto 0);
-			Sel: in std_logic;
-			
-			-- Saídas
-			Saida: out std_logic_vector(31 downto 0)
-		);
-	end component;
-	
-	-- Unidade de controle
-	component control
-		Port(
-			-- Entradas
-			opcode: in std_logic_vector(5 downto 0);
-			
-			-- Saídas
-			alu_op: 	out std_logic_vector(1 downto 0);
-			reg_write:  out std_logic;
-			reg_dst: 	out std_logic;
-			alu_src: 	out std_logic;
-			branch: 	out std_logic;
-			mem_write: 	out std_logic;
-			mem_to_reg: out std_logic;
-			jump: 		out std_logic;
-			mem_read: 	out std_logic
-		);
-	end component;
-	
-	-- Mux de 2 entradas de 5 bits
-	component mux_2_5
-		Port(
-			-- Entradas
-			A: in std_logic_vector(4 downto 0);
-			B: in std_logic_vector(4 downto 0);
-			Sel: in std_logic;
-			
-			-- Saídas
-			Saida: out std_logic_vector(4 downto 0)
-		);
-	end component;
-	
-	-- Banco de registradores
-	component banco_reg
-		Port(
-			-- Entradas
-			reg1_addr: in std_logic_vector(4 downto 0);
-			reg2_addr: in std_logic_vector(4 downto 0);
-			write_data: in std_logic_vector(31 downto 0);
-            reg_write: in std_logic;
-			CLOCK: in std_logic;
-			
-			-- Saídas
-			read_reg1: out std_logic_vector(31 downto 0);
-			read_reg2: out std_logic_vector(31 downto 0)			
-		);
-	end component;
-	
-	-- Extensor de sinal de 16 bits para 32 bits
-	component extensor_sinal 
-		Port(
-			-- Entradas
-			input_16: in std_logic_vector(15 downto 0);
-			
-			-- Saídas
-			output_32: out std_logic_vector(31 downto 0);
-		);
-	end component;
-	
-	-- Unidade de controle da ALU
-	component alu_control
-		Port(
-			-- Entradas
-			funct: in std_logic_vector(5 downto 0);
-			alu_op: in std_logic_vector(1 downto 0);
-			
-			-- Saídas
-			a_inverte: out std_logic;
-			b_inverte: out std_logic;
-			operacao: out std_logic
-		);
-	end component;
-	
-	-- ULA
-	component ULA
-		Port(
-			-- Entradas
-			A: in std_logic_vector(31 downto 0);
-			B: in std_logic_vector(31 downto 0);
-			Anegate: in std_logic;
-			Bnegate: in std_logic;
-			Operacao: in std_logic_vector(1 downto 0);
-			
-			-- Saídas
-			Resultado: out std_logic_vector(31 downto 0)
-		);
-	end component;
-	
-	-- Memória de dados
-	component memDados
-		Port(
-			-- Entradas
-			DadoEscrita: in std_logic_vector(31 downto 0);
-			Endereco: in std_logic_vector(31 downto 0);
-			EscreverMem: in std_logic;
-			CLOCK: in std_logic;
-			LerMem: in std_logic;
-			DebugEndereco: in std_logic_vector(31 downto 0);
-						
-			-- Saídas
-			DadoLido: out std_logic_vector(31 downto 0);
-			DebugPalavra: out std_logic_vector(31 downto 0)
-		);
-	end component;
-	
     -- Fios do PC
 	signal pc_in: std_logic_vector(31 downto 0) := x"00003000";
 	signal pc_out: std_logic_vector(31 downto 0);
@@ -223,20 +43,20 @@ architecture Behavioral of caminho_de_dados is
     -- Fios para a unidade de controle da ULA
     signal a_inverte_cable: std_logic;
     signal b_inverte_cable: std_logic;
-    signal operacao_da_ula_cable: std_logic;
+    signal operacao_da_ula_cable: std_logic_vector(1 downto 0);
     
     -- Fios da ULA
     signal result_zero_cable: std_logic;
     signal ula_result_cable: std_logic_vector(31 downto 0);
     
     -- Fio para identificar o resultado entre a soma do endereço de desvio e o PC
-    signal soma_branch_pc_cable: std_logic;
+    signal soma_branch_pc_cable: std_logic_vector(31 downto 0);
     
     -- Fio para identificar branch
     signal branch_if_eqz_cable: std_logic;
 
 	-- Fio para conectar a saída do primeiro mux do PC
-    signal first_mux_pc: std_logic;
+    signal first_mux_pc: std_logic_vector(31 downto 0);
 
 	-- Fios para os operandos da ULA
     signal first_reg_content: std_logic_vector(31 downto 0);
@@ -247,29 +67,28 @@ architecture Behavioral of caminho_de_dados is
     signal dado_lido_cable: std_logic_vector(31 downto 0);
     
     -- Fio escreve dado
-    signal data_write_cable: std_logic_vector(31 downto 0) := x"00000000"
+    signal data_write_cable: std_logic_vector(31 downto 0);
+	 
+	 -- Fio inicial do pc reset
+	 signal pc_reset_init: std_logic := '1';
 
 begin
-	-- **** TERMINAR DE INSTANCIAR O PC ****
-	Instance_PC: PC 
+    Instance_PC: entity work.PC 
     	Port Map(
-			-- Entradas
-            CLK => clk, -- Colocar o pc para receber o clock
-			
-            clear
-            enable
-            preset
-            reset
+	-- Entradas
+            CLK => clk, 
+	    reset => pc_reset_init,
             
             d_in => pc_in,
             
             -- Saídas
             q_out => pc_out
 		);
-    
+	 pc_reset_init <= '0';
+	 
     -- Incrementador do PC
-    Instance_Somador32bits_PC: Somador32bits
-		Port Map(
+    Instance_Somador32bits_PC: entity work.Somador32bits
+	 Port Map(
         	-- Entradas do incrementador do PC
             A => pc_out,
             B => std_logic_vector(to_unsigned(4, 32)), 
@@ -279,7 +98,7 @@ begin
         );
 	
    
-    Instance_MemoriaInstrucoes: memInstrucoes
+    Instance_MemoriaInstrucoes: entity work.memInstrucoes
     	Port Map(
         	-- Entradas
             Endereco => pc_out,
@@ -289,7 +108,7 @@ begin
         );
     
      
-    Instance_Deslocador2bits_esq_jump: deslocador_2b_esq
+    Instance_Deslocador2bits_esq_jump: entity work.deslocador_2b_esq
     	Port Map(
         	-- Entradas
             entrada => instrucao,
@@ -300,9 +119,9 @@ begin
     
     -- O endereço de jump deslocado recebe os 4 bits mais à esquerda 
     -- do endereço do PC+4
-    jump_adr_from_deslocador_2b_esq(31 downto 28) <= pc_mais_quatro(31 downto 28);
+    --jump_adr_from_deslocador_2b_esq(31 downto 28) <= pc_mais_quatro(31 downto 28);
     
-    Instance_Unidade_de_Controle: control
+    Instance_Unidade_de_Controle: entity work.control
     	Port Map(
         	-- Entradas:
             opcode => instrucao(31 downto 26),
@@ -320,7 +139,7 @@ begin
         );
         
 	
-    Instance_Mux2x5_Sel_Reg: mux_2_X_5
+    Instance_Mux2x5_Sel_Reg: entity work.mux_2_X_5
     	Port Map(
         	-- Entradas
         	A => instrucao(20 downto 16),
@@ -332,7 +151,7 @@ begin
         );
         
     -- Banco de registradores
-    Instance_Banco_Reg: banco_reg
+    Instance_Banco_Reg: entity work.banco_reg
     	Port Map(
         	-- Entradas
             reg1_addr => instrucao(25 downto 21),
@@ -347,7 +166,7 @@ begin
         );
     
     -- Extensão de sinal do imediato de desvio
-    Instance_Extensao_Sinal_EndDesvio: extensor_sinal
+    Instance_Extensao_Sinal_EndDesvio: entity work.extensor_sinal
     	Port Map(
         	-- Entradas
             input_16 => instrucao(15 downto 0),
@@ -357,7 +176,7 @@ begin
         );
     
     -- Alinha o incremento do descvio
-    Instance_Deslocador_2b_esq_alinhamento_desvio: deslocador_2b_esq
+    Instance_Deslocador_2b_esq_alinhamento_desvio: entity work.deslocador_2b_esq
     	Port Map(
         	-- Entradas
             entrada => desvio_addr_pre_alinhamento,
@@ -367,7 +186,7 @@ begin
         );
         
     -- Unidade de controle da ULA
-    Instance_Unidade_de_Controle_ULA: alu_control
+    Instance_Unidade_de_Controle_ULA: entity work.alu_control
     	Port Map(
         	-- Entradas
             funct => instrucao(5 downto 0),
@@ -379,9 +198,8 @@ begin
             operacao => operacao_da_ula_cable
         );
     
-   
     -- Soma o incremento do branch com o PC+4
-   	Instance_Somador32bits_Branch: Somador32bits
+   	Instance_Somador32bits_Branch: entity work.Somador32bits
     	Port Map(
         	-- Entradas
             A => pc_mais_quatro,
@@ -394,7 +212,7 @@ begin
     -- Instancia o primeiro mux que determina o próximo endereço
     -- do PC
     branch_if_eqz_cable <= branch_cable and result_zero_cable;
-   	Instance_Mux2x32_Branch: mux_2_x_32
+   	Instance_Mux2x32_Branch: entity work.mux_2_x_32
     	Port Map(
         	-- Entradas
             A => pc_mais_quatro,
@@ -407,18 +225,18 @@ begin
         
     -- Instancia o segundo mux que determina o próximo endereço
     -- do PC
-	Instance_Mux2x32_Branch_PC_Jump: mux_2_x_32
+	Instance_Mux2x32_Branch_PC_Jump: entity work.mux_2_x_32
     	Port Map(
         	-- Entradas
             A => first_mux_pc,
-            B => jump_adr_from_deslocador_2b_esq,
+            B => pc_mais_quatro(31 downto 0),
             Sel => jump_sel,
             
             -- Saídas
             Saida => pc_in
         );
         
-    Instance_Mux2x32_ULA_2nd_op: mux_2_x_32
+    Instance_Mux2x32_ULA_2nd_op: entity work.mux_2_x_32
     	Port Map(
         	-- Entradas
             A => second_reg_content,
@@ -430,7 +248,7 @@ begin
         );
     
     -- Unidade de Controle (ULA)
-    Instance_ULA: Ula_32
+    Instance_ULA: entity work.Ula_32
     	Port Map(
         	-- Entradas
             A => first_reg_content,
@@ -445,7 +263,7 @@ begin
         );
    	
     -- Memória de dados
-    Instance_memDados: memDados
+    Instance_memDados: entity work.memDados
     	Port Map(
         	-- Entradas
             DadoEscrita => second_reg_content,
@@ -456,12 +274,12 @@ begin
             DebugEndereco => ula_result_cable,
             
             -- Saídas
-        	DadoLido => dado_lido_cable,
-            DebugPalavra => dado_lido_cable
+        	DadoLido => dado_lido_cable
+            --DebugPalavra => dado_lido_cable
         );
     
     -- Último Mux do caminho de dados
-    Instance_Mux2x32_Memtoreg: mux_2_x_32
+    Instance_Mux2x32_Memtoreg: entity work.mux_2_x_32
     	Port Map(
         	-- Entradas
             A => ula_result_cable,
@@ -473,5 +291,3 @@ begin
         );
     
 end Behavioral;
-
-
