664e1dace4033bfdfe9f07a4a896d49cc444e718
Vítor Oliveira Amorim <135454159+Voamorim@users.noreply.github.com>
2025-02-20 22:21:12 -0300

    Atualiza a or de 32 bits

--- or_32.vhd
+++ or_32.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company:
--- Engineer:
---
--- Create Date:    12:02:46 01/20/2025
--- Design Name:
--- Module Name:    ula_1 - Behavioral
--- Project Name:
--- Target Devices:
--- Tool versions:
--- Description:
---
--- Dependencies:
---
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
