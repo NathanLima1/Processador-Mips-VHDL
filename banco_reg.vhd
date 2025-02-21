4b4987ba6ea2eda996d21266598beb1948a2b20a
Vítor Oliveira Amorim <135454159+Voamorim@users.noreply.github.com>
2025-02-20 21:18:07 -0300

    Atualiza o banco de registradores

--- banco_reg.vhd
+++ banco_reg.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: Leonardo Guimarães de Oliveira
--- 
--- Create Date:    15:43:41 01/29/2025 
--- Design Name: 
--- Module Name:    banco_reg - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
