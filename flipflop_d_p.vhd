50bd1192b38bd7dd27dc3b92fd5f39b5b70297d0
Vítor Oliveira Amorim <135454159+Voamorim@users.noreply.github.com>
2025-02-20 21:53:22 -0300

    Atualiza o Flip Flop D com Preset

--- flipflop_d_p.vhd
+++ flipflop_d_p.vhd
@@ -1,29 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: UFSJ
--- Engineer: Nathan Lima
--- 
--- Create Date:    15:17:50 01/22/2025 
--- Design Name: 
--- Module Name:    FlipFlopD - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: FlipFlop D com Preset assincrono e borda de subida
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
---use IEEE.NUMERIC_STD.ALL;
-
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity FlipFlop_D_P is
 	port(
@@ -39,11 +15,11 @@ architecture Behavioral of FlipFlop_D_P is
 begin
 	process (clk)
 	begin
-		if Preset = '1' then
+	if Preset = '1' then
             Q <= '1';
         elsif rising_edge(clk) then
             Q <= D;
-		end if;
+	end if;
 	end process;
 
 end Behavioral;
